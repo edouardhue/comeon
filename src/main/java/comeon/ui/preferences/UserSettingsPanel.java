82564658574d94dbf4923b3122c90e0b46779a86
Édouard Hue <edouard.hue@gmail.com>
2013-04-12 22:35:08 +0200

    Factorisation du code des listeners

--- src/main/java/comeon/ui/preferences/UserSettingsPanel.java
+++ src/main/java/comeon/ui/preferences/UserSettingsPanel.java
@@ -27,7 +27,7 @@ final class UserSettingsPanel extends JPanel {
   private final JTextField loginField;
 
   private final JPasswordField passwordField;
-  
+
   private final User user;
 
   public UserSettingsPanel() {
@@ -40,64 +40,20 @@ final class UserSettingsPanel extends JPanel {
       candidateUser = new User("", "", "");
     }
     this.user = candidateUser;
-    
+
     final JLabel displayNameLabel = new JLabel(UI.BUNDLE.getString("prefs.user.displayName"), SwingConstants.TRAILING);
     this.add(displayNameLabel);
     displayNameField = new JTextField(this.user.getDisplayName(), 20);
     displayNameField.setMaximumSize(displayNameField.getPreferredSize());
-    displayNameField.getDocument().addDocumentListener(new DocumentListener() {
-      
-      @Override
-      public void removeUpdate(final DocumentEvent e) {
-        this.updateUser(e.getDocument());
-      }
-      
-      @Override
-      public void insertUpdate(final DocumentEvent e) {
-        this.updateUser(e.getDocument());
-      }
-      
-      @Override
-      public void changedUpdate(final DocumentEvent e) {
-      }
-      
-      private void updateUser(final Document doc) {
-        try {
-          UserSettingsPanel.this.user.setDisplayName(doc.getText(0, doc.getLength()));
-        } catch (final BadLocationException e) {
-        }
-      }
-    });
+    displayNameField.getDocument().addDocumentListener(new DisplayNameDocumentListener());
     displayNameLabel.setLabelFor(displayNameField);
     this.add(displayNameField);
-    
+
     final JLabel loginLabel = new JLabel(UI.BUNDLE.getString("prefs.user.login"), SwingConstants.TRAILING);
     this.add(loginLabel);
     loginField = new JTextField(this.user.getLogin(), 10);
     loginField.setMaximumSize(loginField.getPreferredSize());
-    loginField.getDocument().addDocumentListener(new DocumentListener() {
-      
-      @Override
-      public void removeUpdate(final DocumentEvent e) {
-        this.updateUser(e.getDocument());
-      }
-      
-      @Override
-      public void insertUpdate(final DocumentEvent e) {
-        this.updateUser(e.getDocument());
-      }
-      
-      @Override
-      public void changedUpdate(final DocumentEvent e) {
-      }
-      
-      private void updateUser(final Document doc) {
-        try {
-          UserSettingsPanel.this.user.setLogin(doc.getText(0, doc.getLength()));
-        } catch (final BadLocationException e) {
-        }
-      }
-    });
+    loginField.getDocument().addDocumentListener(new LoginDocumentListener());
     loginLabel.setLabelFor(loginField);
     this.add(loginField);
 
@@ -105,36 +61,60 @@ final class UserSettingsPanel extends JPanel {
     this.add(passwordLabel);
     passwordField = new JPasswordField(this.user.getPassword(), 10);
     passwordField.setMaximumSize(passwordField.getPreferredSize());
-    passwordField.getDocument().addDocumentListener(new DocumentListener() {
-      
-      @Override
-      public void removeUpdate(final DocumentEvent e) {
-        this.updateUser(e.getDocument());
-      }
-      
-      @Override
-      public void insertUpdate(final DocumentEvent e) {
-        this.updateUser(e.getDocument());
-      }
-      
-      @Override
-      public void changedUpdate(final DocumentEvent e) {
-      }
-      
-      private void updateUser(final Document doc) {
-        try {
-          UserSettingsPanel.this.user.setPassword(doc.getText(0, doc.getLength()));
-        } catch (final BadLocationException e) {
-        }
-      }
-    });
+    passwordField.getDocument().addDocumentListener(new PasswordDocumentListener());
     passwordLabel.setLabelFor(passwordField);
     this.add(passwordField);
 
     SpringUtilities.makeCompactGrid(this, 3, 2, 6, 6, 6, 6);
   }
-  
+
   public User getUser() {
     return user;
+  }
+
+  private abstract class UserDocumentListener implements DocumentListener {
+    @Override
+    public final void removeUpdate(final DocumentEvent e) {
+      this.updateUser(e.getDocument());
+    }
+
+    @Override
+    public final void insertUpdate(final DocumentEvent e) {
+      this.updateUser(e.getDocument());
+    }
+
+    @Override
+    public final void changedUpdate(final DocumentEvent e) {
+    }
+
+    private void updateUser(final Document doc) {
+      try {
+        this.doUpdate(doc.getText(0, doc.getLength()));
+      } catch (final BadLocationException e) {
+      }
+    }
+
+    protected abstract void doUpdate(final String newValue);
+  }
+
+  private final class PasswordDocumentListener extends UserDocumentListener {
+    @Override
+    protected void doUpdate(final String newValue) {
+      UserSettingsPanel.this.user.setPassword(newValue);
+    }
+  }
+
+  private final class LoginDocumentListener extends UserDocumentListener {
+    @Override
+    protected void doUpdate(String newValue) {
+      UserSettingsPanel.this.user.setLogin(newValue);
+    }
+  }
+
+  private final class DisplayNameDocumentListener extends UserDocumentListener {
+    @Override
+    protected void doUpdate(final String newValue) {
+      UserSettingsPanel.this.user.setDisplayName(newValue);
+    }
   }
 }
