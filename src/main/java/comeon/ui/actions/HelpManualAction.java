0267286d265f9cf7edec8653984391771f9c3654
Édouard Hue <edouard.hue@gmail.com>
2016-01-11 00:15:10 +0100

    Finally activating the starter guide button

--- src/main/java/comeon/ui/actions/HelpManualAction.java
+++ src/main/java/comeon/ui/actions/HelpManualAction.java
@@ -1,23 +1,59 @@
 package comeon.ui.actions;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import comeon.ui.UI;
 
 public final class HelpManualAction extends BaseAction {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(HelpManualAction.class);
+
   private static final long serialVersionUID = 1L;
 
-  public HelpManualAction() {
+  public HelpManualAction() throws URISyntaxException {
     super("manual", KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
-    this.setEnabled(false);
   }
 
   @Override
   public void actionPerformed(final ActionEvent e) {
-    // TODO Auto-generated method stub
-
+    try {
+      final URI manualUri = new URI(UI.BUNDLE.getString("manual.url"));
+      if (Desktop.isDesktopSupported()) {
+        final Desktop desktop = Desktop.getDesktop();
+        if (desktop.isSupported(Desktop.Action.BROWSE)) {
+          SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+              try {
+                desktop.browse(manualUri);
+              } catch (final IOException ex) {
+                JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), ex.getLocalizedMessage(), UI.BUNDLE.getString("error.generic.title"), JOptionPane.ERROR_MESSAGE);
+              }
+            }
+          });
+        }
+      }
+    } catch (final URISyntaxException ex) {
+      LOGGER.error("Malformed help manual URI", ex);
+      SwingUtilities.invokeLater(new Runnable() {
+        @Override
+        public void run() {
+          JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), ex.getLocalizedMessage(), UI.BUNDLE.getString("error.generic.title"), JOptionPane.ERROR_MESSAGE);
+        }
+      });
+    }
   }
 
 }
